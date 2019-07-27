4acd18f71d8f667fe2245ef63d864bcd3bcd7de5
Cristian <crissanchez92@gmail.com>
2019-07-27 12:31:52 -0600

    Adding new request component (blank)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewRequestComponent } from './new-request/new-request.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'newrequest', component: NewRequestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewRequestComponent } from './new-request/new-request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewRequestComponent
   ],
   imports: [
     BrowserModule,
